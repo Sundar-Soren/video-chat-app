7532dd892c0244ba8e0a1bcae32b0f3055a4d82f
Sundar Soren <sundarsoren100@gmail.com>
2021-09-02 22:37:39 +0530

    file are added

---
+++ frontend/src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { AppBar, makeStyles, Typography } from "@material-ui/core";
+import VideoPlayer from "./components/VideoPlayer";
+import Options from "./components/Options";
+import Notifications from "./components/Notifications";
+
+const useStyle = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 100px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "600px",
+    border: "2px solid black",
+
+    [theme.breakpoints.down("xs")]: {
+      width: "90%",
+    },
+  },
+  image: {
+    marginLeft: "15px",
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+}));
+const App = () => {
+  const classes = useStyle();
+  return (
+    <div className={classes.wrapper}>
+      <AppBar position="static" className={classes.appBar}>
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+    </div>
+  );
+};
+
+export default App;
